f2b1728db02755e5c02628998ffd17deb74cf535
Nils Patrick Müller <nimuelle@techfak.uni-bielefeld.de>
2021-12-06 12:39:25 +0100

    Add JIT compiler comparison

--- day6/src.py
+++ day6/src.py
@@ -1,7 +1,10 @@
+from functools import partial
 from pathlib import Path
 
+import jax.numpy as jnp
 import numpy as np
-from tqdm import tqdm
+from jax import jit as jjit
+from numba import jit as njit
 
 
 def load_data():
@@ -14,9 +17,22 @@ def load_data():
 
 def proliferate(initial_state, days):
     state = np.bincount(initial_state, minlength=9)
-    for _ in tqdm(range(days)):
+    for _ in range(days):
         state = np.roll(state, -1)
         state[6] += state[8]
+    return state
+
+
+numba_proliferate = njit(proliferate)
+
+
+@partial(jjit, static_argnums=1)
+def jax_proliferate(initial_state, days):
+    arr = jnp.asarray(initial_state, dtype=jnp.int64)
+    state = jnp.bincount(arr, length=9)
+    for _ in jnp.arange(0, days):
+        state = jnp.roll(state, -1)
+        state = state.at[6].set(state[6] + state[8])
     return state
 
 
